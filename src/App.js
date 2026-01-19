70bf48b109a9fdb8e8452e8ddc2ecdabde64585e
huizh11 <hanghuizhi@gmail.com>
2026-01-19 09:46:21 +0800

    commit base app.js code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,74 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import CardList from "./pages/CardList";
-import AddCard from "./pages/AddCard";
-import EditCard from "./pages/EditCard";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import { FlatList, StatusBar, Text, TextInput, View, Image } from 'react-native';
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cards" element={<CardList />} />
-        {/* TODO: Complete the routes */}
-      </Routes>
-    </BrowserRouter>
-  );
-}
+//Create a new variable named OriginalData
+let originalData = []
+
+const App = () => {
+    const [mydata, setMydata] = useState([]);
+
+    useEffect(() => {
+        // Add fetch() - Exercise 1A
+        const myurl = "https://onlinecardappwebservice-anqr.onrender.com/allcards";
+
+        fetch(myurl)
+            .then((response) => response.json())
+            .then((myJson) => {
+                setMydata(myJson);
+                originalData = myJson;
+            });
+    }, []); //
+
+    const FilterData = (text) => {
+        if (text !== '') {
+            let myFilteredData = originalData.filter((item) =>
+                item.card_name.toLowerCase().includes(text.toLowerCase())
+            );
+            setMydata(myFilteredData);
+        } else {
+            setMydata(originalData);
+        }
+    };
+
+
+    const renderItem = ({ item }) => {
+        return (
+            <View
+                style={{
+                    flexDirection: 'row',
+                    borderWidth: 1,
+                    margin: 10,
+                    padding: 10,
+                    alignItems: 'center',
+                    justifyContent: 'space-between'
+                }}
+            >
+                <Text style={{ fontSize: 16 }}>
+                    {item.card_name}
+                </Text>
+
+                <Image
+                    source={{ uri: item.card_pic }}
+                    style={{ width: 80, height: 110 }}
+                    resizeMode="contain"
+                />
+
+            </View>
+        );
+    };
+
+
+    return (
+        <View>
+            <StatusBar />
+            <Text>Search:</Text>
+            <TextInput
+                style={{ borderWidth: 1 }}
+                onChangeText={(text) => { FilterData(text); }}
+            />
+            <FlatList data={mydata} renderItem={renderItem} />
+        </View>
+    );
+};
+
+export default App;
